804689e79e634d7fee70892188441876b1d038df
kayque <kayque-ferreira@hotmail.com.br>
2021-07-11 15:30:16 -0400

    começando aplicação

---
+++ src/main/java/com/digitalinovationone/springboot/controler/HelloControler.java
@@ -0,0 +1,19 @@
+package com.digitalinovationone.springboot.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloControler {
+    @GetMapping("/hello1")
+    public String mensagem(){
+        return "hello satan";
+    }
+    @GetMapping("/hello2")
+    public String mensagem1(){
+        return "caralha";
+    }
+
+}
